f2de6b480633eb82cdb75107d1595c8cb253e123
Brendan Schlaman <brendan.schlaman@gmail.com>
2022-03-06 16:34:12 -0500

    query parameter parsing for number of data objects

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 This tools is a wrapper around `github.com/bxcodec/faker/v3` and allows for quick test data generation with a simple GET request.
 ## Usage
 By default, data-generator-server runs on http port 8080.  The request path is:
-`GET /data?n=N` where `N` is the number of records you want to generate.  The result will be returned as a JSON array.
+`GET /data?n=N` where `N` is the number of records you want to generate (currently up to 1M).  The result will be returned as a JSON array.
 ## Schema
 The schema is currently fixed, but this may change in the future.
 ```json
@@ -14,4 +14,5 @@ The schema is currently fixed, but this may change in the future.
 	"email": "bs@foo.xyz"
 }]
 ```
-
+## TODO
+- make data generation concurrent
--- main.go
+++ main.go
@@ -4,34 +4,41 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/bschlaman/b-utils/pkg/utils"
 	"github.com/bxcodec/faker/v3"
 )
 
 const (
-	serverName string = "Data-Generator"
-	port       string = ":8080"
-	configPath string = "config.json"
+	serverName     string = "Data-Generator"
+	port           string = ":8080"
+	configPath     string = "config.json"
+	maxDataObjects int    = 1000000
 )
 
+type dataSchema struct {
+	Timestamp string `faker:"timestamp" json:"timestamp"`
+	FirstName string `faker:"first_name" json:"fname"`
+	LastName  string `faker:"last_name" json:"lname"`
+	Email     string `faker:"email" json:"email"`
+}
+
 func genData() (interface{}, error) {
-	data := struct {
-		Timestamp string `faker:"timestamp" json:"timestamp"`
-		FirstName string `faker:"first_name" json:"fname"`
-		LastName  string `faker:"last_name" json:"lname"`
-		Email     string `faker:"email" json:"email"`
-	}{}
-	if err := faker.FakeData(&data); err != nil {
+	ds := dataSchema{}
+	if err := faker.FakeData(&ds); err != nil {
 		return nil, err
 	}
-	return data, nil
+	return ds, nil
 }
 
 func genDataMany(n int) ([]interface{}, error) {
 	data := make([]interface{}, n)
 	for i := 0; i < n; i++ {
-		d, _ := genData()
+		d, err := genData()
+		if err != nil {
+			return nil, err
+		}
 		data[i] = d
 	}
 	return data, nil
@@ -40,8 +47,28 @@ func genDataMany(n int) ([]interface{}, error) {
 // Handlers
 func dataHandle() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		data, _ := genDataMany(2)
-		res, _ := json.Marshal(&data)
+		// default to 1 data object
+		numDataObjects := 1
+		parsed, err := strconv.Atoi(r.URL.Query().Get("n"))
+		if err == nil && parsed > 0 {
+			numDataObjects = parsed
+		}
+
+		if numDataObjects > maxDataObjects {
+			http.Error(w, fmt.Sprintf("can only request up to %d objects", maxDataObjects), http.StatusBadRequest)
+			return
+		}
+
+		data, err := genDataMany(numDataObjects)
+		if err != nil {
+			http.Error(w, "something went wrong", http.StatusBadRequest)
+			return
+		}
+		res, err := json.Marshal(&data)
+		if err != nil {
+			http.Error(w, "something went wrong", http.StatusBadRequest)
+			return
+		}
 		w.Header().Set("Content-Type", "application/json")
 		fmt.Fprintf(w, string(res))
 	})
@@ -53,9 +80,8 @@ func initHandlers() {
 }
 
 func main() {
-	fmt.Println("starting", serverName)
+	fmt.Println("starting", serverName, "on port", port)
 	initHandlers()
 	fmt.Println("initialized handlers")
 	http.ListenAndServe(port, nil)
-	fmt.Println("started", serverName, "on port", port)
 }
